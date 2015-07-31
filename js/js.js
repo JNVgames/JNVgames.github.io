e223e0b519feec80d773269bc6be1e5ddc63043c
JarninFang <jarninfang@gmail.com>
2015-07-31 02:13:12 -0700

    Added in easing effect on learn more button

---
+++ js/js.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('#learn-button').bind('click', function(event) {
+		var $anchor = $(this);
+
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+
+		event.preventDefault();
+	});
+});
